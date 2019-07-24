0a3fae73819b579098cb20c1979904fb4913ac12
SillyChill <grumark198@gmail.com>
2019-07-24 12:12:07 +0300

    Добавил фильтрацию

--- Assets/Scripts/Script.js
+++ Assets/Scripts/Script.js
@@ -78,18 +78,24 @@ var res_arr = [
 var time_t = 0;
 var t = 0;
 
-function add_card() {
-    if ( i >= res_arr.length)
-    return 0;   
-    var j = 0;
+function add_card(a) {
+	if (a == res_arr)
+	{
+    	if ( i >= res_arr.length)
+    	return 0;   
+    	var j = 0;
     
-    while (j < 4)
-    {
-        var parentElem = document.getElementById('Front_Page_Grid');
-        var div_card = document.createElement('div');
-            div_card.className = "Front_Card";
-            div_card.innerHTML = 
-                `<div class="article_card">
+    	while (j < 4)
+    	{
+    		if (j > res_arr.length)
+    		{
+    			return 0;	
+    		}
+        	var parentElem = document.getElementById('Front_Page_Grid');
+        	var div_card = document.createElement('div');
+            	div_card.className = "Front_Card";
+            	div_card.innerHTML = 
+                	`<div class="article_card">
                         <a class="article_card_link">
                             <div class="image_wrap">
                                 <picture>
@@ -113,57 +119,137 @@ function add_card() {
                             </div>
                             <div class="image_card"></div>
                         </a>
-                </div>`;
-        parentElem.appendChild(div_card);	
-        t++;
-        j++;
-        i++;
-    };
-    time_t = t;
-    console.log(time_t);
-    return i;
+                	</div>`;
+        	parentElem.appendChild(div_card);	
+        	t++;
+        	j++;
+        	i++;
+    	}
+    }
+    if (a == filt_result)	
+    {
+    	if ( i >= filt_result.length )
+    	return 0;   
+    	var j = 0;
+    
+    	while (j < 4)
+    	{
+    		if ( i >= filt_result.length ){
+    			return 0;
+    		}
+
+        		var parentElem = document.getElementById('Front_Page_Grid');
+        		var div_card = document.createElement('div');
+            		div_card.className = "Front_Card";
+            		div_card.innerHTML = 
+                		`<div class="article_card">
+                        	<a class="article_card_link">
+                            	<div class="image_wrap">
+                                	<picture>
+                                    	<img class="article_card_image" src="${filt_result[i].img}")>
+                                	</picture>
+                            	</div>
+                            	<div class="article_card_content">
+                                	<div class="main_tag">Блоги</div>
+                                	<div class="card_title">${filt_result[i].s_t}</div>
+                                	<div class="article_card_stats">
+                                    	<div class="read_time">
+                                        	<div class="read_time_icon">
+                                            	<div class="svg_icon">
+                                                	<i class="far fa-clock"></i>
+                                            	</div>
+                                        	</div>
+                                        	<span class="article_card_counter">${filt_result[i].read_t}</span>
+                                    	</div>
+                                    	<span id="data_${t}" class="article_card_data">${filt_result[i].date_t}</span>
+                                	</div>
+                            	</div>
+                            	<div class="image_card"></div>
+                        	</a>
+                		</div>`;
+        		parentElem.appendChild(div_card);	
+        		t++;
+        		j++;
+        		i++;
+        	
+    	}
+    	time_t = t;
+   		console.log(time_t);
+    	return i;
+	}
 };
 
 
 $(document).ready(function(){
-	add_card();
+	add_card(res_arr);
 });
 
-var heightsBox = 0;
-function add_card_height() {
-    add_card();
-    $('.Front_Card').each(function(){
+
+function height_options(){
+	var heightsBox = 0;
+	$('.Front_Card').each(function(){
         let cardTitleHeight = $(this).find('.card_title').height();
-        // console.log(cardTitleHeight);
         if ( cardTitleHeight > heightsBox){
             heightsBox = cardTitleHeight;
         }
     });
-    // console.log(heightsBox);
     $('.Front_Card').find('.card_title').height(heightsBox);
 }
+
+function add_card_with_height() {
+	if ( filt_click == false){
+    	add_card(res_arr);
+    	height_options();
+	}
+	else
+	{
+		add_card(filt_result);
+    	height_options();
+	}
+}
+
 var c;
 function sort_card(){
-
+	filt_click = false;
 	res_arr.sort(function(a,b) 
 	{ 
      return  Date.parse(a.date_t) -  Date.parse(b.date_t) 
 	});
-
 	$('.Front_Card').remove();
-
 	i = 0;
-	add_card();
-
-	heightsBox = 0;
-	$('.Front_Card').each(function(){
-        let cardTitleHeight = $(this).find('.card_title').height();
-        // console.log(cardTitleHeight);
-        if ( cardTitleHeight > heightsBox){
-            heightsBox = cardTitleHeight;
-        }
-    });
-    $('.Front_Card').find('.card_title').height(heightsBox);
-
-
+	add_card(res_arr);
+	height_options();
 };
+
+
+var filt_result = [];
+var arr_f_s = [];
+var filt_click = false;
+ function filt_card(){
+ 	filt_click = true;
+ 	var v1 = document.getElementById('input_dat_1').value;
+	var v2 = document.getElementById('input_dat_2').value;
+
+ 	res_arr.sort(function(a,b) 
+	{ 
+     return  Date.parse(a.date_t) -  Date.parse(b.date_t) 
+	});
+	$('.Front_Card').remove();
+	i = 0;
+	var n;
+	var f1 = Date.parse(v1);
+	var f2 = Date.parse(v2);
+	for (n = 0; n < res_arr.length; n++){
+		var arr_f = Date.parse(res_arr[n].date_t);
+		if (arr_f >= f1 && arr_f <= f2){
+			arr_f_s[n] = res_arr[n];
+		}
+	};
+	// filt_arr_fix(arr_f_s);
+	filt_result = arr_f_s.filter(x => x != null);
+	console.log(filt_result);
+   	add_card_with_height();
+   	height_options();
+   	
+
+ };		
